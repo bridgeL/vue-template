acd5383639d377766cc69ad3aa2009231571e912
bridgeL <47290820+bridgeL@users.noreply.github.com>
2021-12-24 17:35:36 +0800

    Create vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+// vue.config.js
+
+/**
+ * @type {import('@vue/cli-service').ProjectOptions}
+ */
+module.exports = {
+    // 详细配置查询vue cli官网
+    // publicPath默认值为'/' 默认架设在根路径上
+    // 修改为'./'后可以将网页部署在任意位置
+    publicPath: './'
+}
